b8b0eb11107b6de16747937668581ab483841a46
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-01-31 21:33:22 +0500

    Update Q2.cpp

--- Labs/01/Q2.cpp
+++ Labs/01/Q2.cpp
@@ -35,3 +35,43 @@ int main(){
         cout << "\nStudent " << i+1 << " - Total marks: " << data.total_marks << "\t|\t" << "Average: " << data.average << "\t|\t" << "Grade: " << data.grade << "\t|\t" << endl;
     }
 }
+
+// #include <iostream>
+// #include <iomanip>
+
+// using namespace std;
+
+// char calculateGrade(float average){
+//     if (average >= 90){
+//         return 'A';
+//     } else if (average >= 80){
+//         return 'B';
+//     } else if (average >= 70){
+//         return 'C';
+//     } else if (average >= 60){
+//         return 'D';
+//     } else{
+//         return 'F';
+//     }
+// }
+
+// int main() {
+//     int numStudents;
+
+//     cout << "Enter the number of students: ";
+//     cin >> numStudents;
+
+//     for (int i = 1; i <= numStudents; ++i){
+//         float math, english, science;
+
+//         cout << "Enter marks for Student " << i << " (Mathematics English Science): ";
+//         cin >> math >> english >> science;
+
+//         float total = math + english + science;
+//         float average = total / 3;
+
+//         cout << "Student " << i << " - Marks: " << setw(5) << static_cast<int>(math) << setw(5) << static_cast<int>(english) << setw(5) << static_cast<int>(science)
+//              << " | Total: " << setw(5) << static_cast<int>(total) << " | Average: " << fixed << setprecision(2) << average
+//              << " | Grade: " << calculateGrade(average) << endl;
+//     }
+// }
