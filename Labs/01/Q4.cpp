ed26929abb7d3ae5d8936e5ab5035fd9d70e0520
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-02-02 18:57:48 +0500

    Update Q4.cpp

--- Labs/01/Q4.cpp
+++ Labs/01/Q4.cpp
@@ -1,1 +1,22 @@
-
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 9, max1 = 0,max2 = 0, count = -1;
+    int height[n] = {1,8,6,2,5,4,8,3,7};
+    int area;
+    for(int i=0; i<n; i++){
+        if(height[i] >= max1){ 
+        max1 = height[i];
+        height[i] = 0;
+        }
+    }
+    for(int j=n-1; j>0; j--){ 
+        if(height[j] > max2) max2 = height[j];
+        }
+    for(int k=0; k<n; k++){
+        if(height[k] != max2) count++;
+        if(height[k] == max2) break;
+    }
+    area = count*max2;
+    cout<< area;
+}
