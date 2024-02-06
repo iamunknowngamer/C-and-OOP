0d10af4975cd6d7c787724824df19785ec4fb8b4
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-02-06 09:03:56 +0500

    Update Q3.cpp

--- Labs/02/Q3.cpp
+++ Labs/02/Q3.cpp
@@ -6,19 +6,20 @@ Roll number: 23k-0032
 #include <iostream>
 using namespace std;
 
-bool findSubset(int arr[], int n, int target){
-    for(int i=0; i<n-1; i++){
-        for(int j=i+1; j<n; j++){
-            if(arr[i]+arr[j]==target) return true;
-        }
+bool findSubset(int arr[], int n, int target,int i, int j){
+    if(i==n) return false;
+    if(j>n-1){
+        i++;
+        j=i+1;
     }
-    return false;
+    if(arr[i]+arr[j]==target) return true;
+    else return findSubset(arr,n,target,i,j+1);
 }
 
 int main(){
     int n=5;
     int arr[n] = {1,2,3,4,5};
-    int target = 9;
-    if(findSubset(arr,n,target)) cout<<"Subset found";
+    int target = 7;
+    if(findSubset(arr,n,target,0,1)) cout<<"Subset found";
     else cout<<"Subset not found";
 }
