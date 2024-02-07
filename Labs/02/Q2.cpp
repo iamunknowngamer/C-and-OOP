82acb3fd04ffe50ec19569fa9c7e69aeb52bbe76
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-02-07 11:18:45 +0500

    Update Q2.cpp

--- Labs/02/Q2.cpp
+++ Labs/02/Q2.cpp
@@ -8,10 +8,60 @@ struct book{
     string genre;
 };
 
+void add(book data[], int &n){
+    int N;
+    cout<<"How many books you wish to add: "; cin>>N;
+    for(int i=n;i<n+N;i++){
+        cout<<"\n-Adding details for book "<<i+1<<": ";
+        cout<<"\nEnter the book title: "; 
+        cin>>data[i].title;
+        cout<<"\nEnter the book author: ";
+        cin>>data[i].author;
+        cout<<"\nEnter the year of publication: ";
+        cin>>data[i].year;
+        cout<<"\nEnter the genre of book: ";
+        cin>>data[i].genre;
+    }
+    n+=N;
+}
+
+void update(book data[], int n){
+    int year;
+    cout<<"Enter the year of the book, you want to update: "; cin>>year;
+    for(int i=0;i<n;i++){
+        if(data[i].year==year){
+            cout<<"Updating details for book "<<i+1<<": \n";
+            cout<<"\nEnter The New Book Title: "; 
+            cin>>data[i].title;
+            cout<<"\nEnter the book author: ";
+            cin>>data[i].author;
+            cout<<"\nEnter the year of publication: ";
+            cin>>data[i].year;
+            cout<<"\nEnter the genre of book: ";
+            cin>>data[i].genre;
+        }
+    }
+}
+
+void search(book data[], int n){
+    string temp;
+    cout<<"\nEnter the (author/title) of the book you want to search for: "; cin>>temp;
+    cout<<"Search complete(0.198 ms)--Matching results are: "<<endl;
+    for(int i=0;i<n;i++){
+        if(data[i].title == temp || data[i].author == temp){
+            cout<<"\nSimilar Result - Book "<<i+1<<":";
+            cout<<"\nBook title: "<<data[i].title;
+            cout<<"\nBook author: "<<data[i].author;
+            cout<<"\nYear of publication: "<<data[i].year;
+            cout<<"\nGenre: "<<data[i].genre<<"\n\n"; 
+        }
+    }
+}
+
 int main(){
-    int n,N;
+    int n;
     cout<<"How many books you want to register: "; cin>>n;
-    book data[n];
+    book data[50];
     for(int i=0;i<n;i++){
         cout<<"\n-Adding details for book "<<i+1<<": ";
         cout<<"\nEnter the book title: "; 
@@ -28,56 +78,17 @@ int main(){
         cout<<"1.Add\n2.Update\n3.Search\n4.Exit"<<endl;
         cin>>choice;
         if(choice==1){
-            cout<<"How many books you wish to add: "; cin>>N;
-            book data[n+N];
-            for(int i=n;i<n+N;i++){
-                cout<<"\n-Adding details for book "<<i+1<<": ";
-                cout<<"\nEnter the book title: "; 
-                cin>>data[i].title;
-                cout<<"\nEnter the book author: ";
-                cin>>data[i].author;
-                cout<<"\nEnter the year of publication: ";
-                cin>>data[i].year;
-                cout<<"\nEnter the genre of book: ";
-                cin>>data[i].genre;
-                }
-            n+=N;
-            }
+            add(data,n);
+        }
         if(choice==2){
-            int year;
-            cout<<"Enter the year of the book, you want to update: ";
-            cin>>year;
-            for(int i=0;i<n;i++){
-                if(data[i].year==year){
-                    cout<<"Updating details for book "<<i+1<<": \n";
-                    cout<<"\nEnter The New Book Title: "; 
-                    cin>>data[i].title;
-                    cout<<"\nEnter the book author: ";
-                    cin>>data[i].author;
-                    cout<<"\nEnter the year of publication: ";
-                    cin>>data[i].year;
-                    cout<<"\nEnter the genre of book: ";
-                    cin>>data[i].genre;
-                }
-            }
+            update(data,n);
         }
         else if(choice==3){
-            string temp;
-            cout<<"\nEnter the (author/title) of the book you want to search for: "; cin>>temp;
-            cout<<"Search complete(0.198 ms)--Matching results are: "<<endl;
-                for(int i=0;i<n;i++){
-                    if(data[i].title == temp || data[i].author == temp){
-                        cout<<"\nSimilar Result "<<i+1<<":";
-                        cout<<"\nBook title: "<<data[i].title;
-                        cout<<"\nBook author: "<<data[i].author;
-                        cout<<"\nYear of publication: "<<data[i].year;
-                        cout<<"\nGenre: "<<data[i].genre<<"\n\n"; 
-                    }
-                }
-            }
+            search(data,n);
+        }
         else if(choice==4){
             cout<<"Program Terminated.";
             exit;
-            }
+        }
     }while(choice!=4);
 }
