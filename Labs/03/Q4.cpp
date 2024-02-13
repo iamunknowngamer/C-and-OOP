afccd259aead3847556bd2caf44de62b45cd407a
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-02-13 10:39:13 +0500

    Update Q4.cpp

--- Labs/03/Q4.cpp
+++ Labs/03/Q4.cpp
@@ -27,6 +27,19 @@ class Smartphone{
         cout<<"The ROM is "<<ROM<<" GB."<<endl;
         cout<<"The storage is "<<storage<<" GB."<<endl;
     }
+
+    void makephonecall(string number){
+        cout<<"Dailing "<<number<<"..."<<endl;
+    }
+    void send_message(string n1, string n2){
+        cout<<"Sending message from "<<n1<<" to "<<n2<<"..."<<endl;
+    }
+    void connect_wifi(string wifi){
+        cout<<"Connected to "<<wifi<<endl;
+    }
+    void web_surfing(string web){
+        cout<<"Browsing on "<<web<<endl;
+    }
 };
 
 
@@ -36,4 +49,8 @@ int main(){
     
     a1.setter("Nokia","N-3310","69x69", 4, 10 , 100);
     a1.getter();
+    a1.makephonecall("0314771414");
+    a1.send_message("03067584773", "03327851810");
+    a1.connect_wifi("StormFibre");
+    a1.web_surfing("Opera Gx");
 }
