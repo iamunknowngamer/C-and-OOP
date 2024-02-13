6a25f28594f4ab7b2981c980c63c36479a2ab2fb
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-02-13 09:57:59 +0500

    Update Q4.cpp

--- Labs/03/Q4.cpp
+++ Labs/03/Q4.cpp
@@ -11,43 +11,21 @@ class Smartphone{
     int storage;
     
     public:
-    void setbrand(string Brand){
+    void setter(string Brand, string Model, string display, int ram, int rom, int Storage){
         brand = Brand;
-    }
-    void setmodel(string Model){
         model = Model;
-    }
-    void setdisplay(string display){
         display_resolution = display;
-    }
-    void setRAM(int ram){
         RAM = ram;
-    }
-    void setROM(int rom){
         ROM = rom;
-    }
-    void setstorage(int Storage){
         storage = Storage;
     }
-    
-    
-    string getbrand(){
-        return brand;
-    }
-    string getmodel(){
-        return model;
-    }
-    string getdisplay(){
-        return display_resolution;
-    }
-    int getRAM(){
-        return RAM;
-    }
-    int getROM(){
-        return ROM;
-    }
-    int getstorage(){
-        return storage;
+    void getter(){
+        cout<<"The brand name is "<<brand<<"."<<endl;
+        cout<<"The model is "<<model<<"."<<endl;
+        cout<<"The resolution is "<<display_resolution<<" pixels."<<endl;
+        cout<<"The RAM is "<<RAM<<" GB."<<endl;
+        cout<<"The ROM is "<<ROM<<" GB."<<endl;
+        cout<<"The storage is "<<storage<<" GB."<<endl;
     }
 };
 
@@ -56,18 +34,6 @@ int main(){
     
     Smartphone a1;
     
-    a1.setbrand("Nokia");
-    a1.setmodel("N-3310");
-    a1.setdisplay("40x50");
-    a1.setRAM(4);
-    a1.setROM(10);
-    a1.setstorage(1000);
-    
-    
-    cout<<"The brand name is "<<a1.getbrand()<<"."<<endl;
-    cout<<"The model is "<<a1.getmodel()<<"."<<endl;
-    cout<<"The resolution is "<<a1.getdisplay()<<" pixels."<<endl;
-    cout<<"The RAM is "<<a1.getRAM()<<" GB."<<endl;
-    cout<<"The ROM is "<<a1.getROM()<<" GB."<<endl;
-    cout<<"The storage is "<<a1.getstorage()<<" GB."<<endl;
+    a1.setter("Nokia","N-3310","69x69", 4, 10 , 100);
+    a1.getter();
 }
