99fb3ef2180591f14dcbd157329012f411da41c2
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-02-22 20:25:22 +0500

    Create Q5.cpp

---
+++ Labs/04/Q5.cpp
@@ -0,0 +1,27 @@
+//Syed Saif ur Rehman Shah (23k-0032)
+
+#include <iostream>
+#include <array>
+using namespace std;
+
+class Office{
+    string Location="";
+    int SeatingCapacity=0;
+    string *Furniture=nullptr;
+
+    public:
+    Office(string Location="", int SeatingCapacity=0, int size=3) : 
+    Location(Location), SeatingCapacity(SeatingCapacity){
+    Furniture = new string[size];
+    }
+
+    ~Office(){
+        cout<<"Memory freed successfully"<<endl;
+        delete[] Furniture;
+    }
+};
+
+int main(){
+    Office* o1= new Office("",0,5);
+    delete o1;
+}
