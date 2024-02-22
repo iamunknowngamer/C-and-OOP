21da1315ad90f4a4ecd73abda7bcfd8deaed4e07
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-02-22 15:24:29 +0500

    Create Q4.cpp

---
+++ Labs/05/Q4.cpp
@@ -0,0 +1,49 @@
+/* Syed Saif ur Rehman Shah (23k-0032) */
+
+#include <iostream>
+using namespace std;
+
+class BankAccount{
+    int accountNumber;
+    string accountHolderName;
+    double balance;
+    
+    public:
+    BankAccount(int accountNumber, string accountHolderName, int balance){
+        this->accountNumber = accountNumber;
+        this->accountHolderName = accountHolderName;
+        this->balance = balance;
+    }
+    
+    void deposit(double deposit){
+        balance += deposit;
+        cout<<"\ncurrent balance: "<<balance;
+    }
+    
+    void withdraw(double withdraw){
+        balance -= withdraw;
+        cout<<"\ncurrent balance: "<<balance;
+    }
+    
+    void display(int i){
+            cout<<"\n\nDetails of Account "<<i+1<<": ";
+            cout<<"\nAccount Number: "<<accountNumber;
+            cout<<"\nAccount Holder Name: "<<accountHolderName;
+            cout<<"\nAccount Balance: "<<balance;
+    }
+};
+
+int main(){
+    BankAccount data[3] = {
+        BankAccount(12345, "Saif", 1000.0),
+        BankAccount(67890, "Abdul", 500.0),
+        BankAccount(11223, "Talha", 1500.0)
+    };
+    
+    for(int i=0; i<3; i++){
+        data[i].display(i);
+    }
+    int acc; cout<<"\n\nWhich account you want to deposit/withdraw: "; cin>>acc;
+    data[acc].deposit(500.0);
+    data[acc].withdraw(300.0);
+}
