89a2707fd1bd7b07cd0de96a41f294a46286dda3
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-02-22 18:54:18 +0500

    Update Q3.cpp

--- Labs/04/Q3.cpp
+++ Labs/04/Q3.cpp
@@ -8,7 +8,7 @@ class WeekDays{
     int CurrentDay;
     
     public:
-    WeekDays(void){
+    void initialize(string days[7]){
         days[0] = "Sunday";
         days[1] = "Monday";
         days[2] = "Tuesday";
@@ -17,24 +17,29 @@ class WeekDays{
         days[5] = "Friday";
         days[6] = "Saturday";
     }
+    WeekDays(void){
+        initialize(days);
+        CurrentDay=0;
+    }
     WeekDays(int day): CurrentDay(day){
-        if(CurrentDay>6) CurrentDay %= CurrentDay;
+        if(CurrentDay>6) this->CurrentDay %= CurrentDay;
+        initialize(days);
     }
     void getCurrentDay(){
-        cout<<"\ncurrent day: "<<days[CurrentDay];
+        cout<<"\nCurrent day: "<<days[CurrentDay];
     }
     void getNextDay(){
-        if(CurrentDay++ > 6) cout<<"\ncurrent day: "<<days[0];
-        else cout<<"\ncurrent day: "<<days[CurrentDay];
+        if(CurrentDay+1 > 6) cout<<"\nNext day: "<<days[0];
+        else cout<<"\nNext day: "<<days[CurrentDay+1];
     }
     void getPreviousDay(){
-        if(CurrentDay-1 < 0) cout<<"\ncurrent day: "<<days[6];
-        else cout<<"\ncurrent day: "<<days[CurrentDay];
+        if(CurrentDay-1 < 0) cout<<"\nPrevious day: "<<days[6];
+        else cout<<"\nPrevious day: "<<days[CurrentDay-1];
     }
     void getNthDayFromToday(){
-        int n=3; CurrentDay += n;
-        if(CurrentDay>6) CurrentDay %= CurrentDay;
-        cout<<"\ncurrent day: "<<days[CurrentDay];
+        int n=3; CurrentDay+=n;
+        if(CurrentDay>6) CurrentDay = CurrentDay%7;
+        cout<<endl<<n<<" days from current day: "<<days[CurrentDay];
     }
 };
 
