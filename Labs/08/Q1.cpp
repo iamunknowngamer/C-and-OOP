0333edfd241eb1dd999e7b422c905b36caa5bd86
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-03-24 09:21:12 +0500

    Update Q1.cpp

--- Labs/08/Q1.cpp
+++ Labs/08/Q1.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstring>
+using namespace std;
 
+class Book{
+    public:
+    string title;
+    string author; 
+    string publisher;
+    
+    Book(string title, string author, string publisher) : title(title), author(author), publisher(publisher) {}
+    
+    void display(){
+        cout << "Title = " << this->title 
+        << "\nAuthor = " << this->author 
+        << "\nPublisher = " << this->publisher << endl;
+    }
+};
+
+class FictionBook : public Book{
+    public:
+    string genre;
+    string protagonist;
+    
+    FictionBook(string title, string author, string publisher, string genre, string protagonist) : Book(title, author, publisher), genre(genre), protagonist(protagonist) {}
+    
+    void display(){
+        Book::display();
+        cout << "Genre = " << genre
+        << "\nProtagonsit = " << protagonist << endl;
+    }
+};
+
+int main(){
+    FictionBook b1("Sword Art Online", "Akira Tomoya", "Izumi", "Magic", "Kirito");
+    b1.display();
+}
