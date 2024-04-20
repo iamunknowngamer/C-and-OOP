4b5c4def0e8f0d96f8872f53202dbf85860552cf
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-04-20 10:35:42 +0500

    Update Q1.cpp

--- Labs/10/Q1.cpp
+++ Labs/10/Q1.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+using namespace std;
 
+class Logo{
+
+    string logo;
+
+    public:
+    Logo(string logo):logo(logo){}
+
+    friend class Platform;
+};
+class Platform{
+public:
+    void display(Logo &obj){
+        obj.logo = "Doge Coin";
+        cout<<obj.logo;
+    }
+};
+class Server{};
+
+int main(){
+    Logo logo("Blue Bird");
+    Platform plat;
+    plat.display(logo);
+    return 0;
+}
